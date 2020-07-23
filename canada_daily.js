b380815c28fd21002c785478b38e4ab7284af950
heldernecker <sumeck@gmail.com>
2020-07-23 13:31:02 -0700

    First commit

---
+++ canada_daily.js
@@ -0,0 +1,68 @@
+const api_key = '61336cd6-47ff-4494-a02c-7fd5aa7c795e';
+
+const api_url = "http://api.coronatracker.com/v3/analytics/newcases/country?countryCode=CA&startDate=2020-01-01&endDate=2020-07-20";
+
+let data = d3.json(api_url, function(error, d) { return d; });
+
+data.then(function(result) {
+  console.log(result);
+
+  var data = result;
+
+  data.forEach(function(d){ 
+    d.last_updated = d.last_updated.slice(0, 10);
+    d.last_updated = d3.timeParse("%Y-%m-%d")(d.last_updated);
+  });
+
+  // if (error) throw error;
+
+  var svg = d3.select("svg"),
+      margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom,
+      g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var x = d3.scaleTime()
+      .rangeRound([0, width]);
+
+  var y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+
+  var line = d3.line()
+      .x(function(d) { return x(d.last_updated); })
+      .y(function(d) { return y(d.new_infections); });
+      
+  x.domain(d3.extent(data, function(d) { 
+    //console.log(d.last_updated);
+    return d.last_updated;
+  }));
+
+  y.domain(d3.extent(data, function(d) {
+    //console.log(d.new_infections);
+    return d.new_infections;
+  }));
+
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    .select(".domain");
+
+  g.append("g")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("New Cases");
+
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+})
---
+++ usa_states.js
@@ -0,0 +1,55 @@
+const width = 1600;
+const height = 800;
+const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+
+const usa_states = 56;
+
+const svg = d3.select('#d3-container')
+  .append('svg')
+  .attr('height', height - margin.top - margin.bottom)
+  .attr('width', width - margin.left - margin.right)
+  .attr('viewBox', [0, 0, width, height]);
+
+const x = d3.scaleBand()
+  .domain(d3.range(usa_states))
+  .range([margin.left, width - margin.right])
+  .padding(0.1);
+
+const y = d3.scaleLinear()
+  .domain([0, 500000])
+  .range([height - margin.bottom, margin.top]);
+
+let data = d3.json("https://covidtracking.com/api/v1/states/current.json", function(error, d) { return d; });
+
+data.then(function(result) {
+  console.log(result);
+
+  svg
+    .append("g")
+    .attr('fill', 'royalblue')
+    .selectAll('rect')
+    .data(result)
+    .join('rect')
+      .attr('x', (d, i) => x(i))
+      .attr('y', (d) => y(d.positive))
+      .attr('height', d => y(0) - y(d.positive))
+      .attr('width', x.bandwidth())
+      .attr('class', 'rectangle');
+
+  function xAxis(g) {
+    g.attr('transform', `translate(0, ${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => result[i].state))
+      .attr('font-size', '12px');
+  }
+
+  function yAxis(g) {
+    g.attr('transform', `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y).ticks(null, result.format))
+      .attr('font-size', '20px');
+  }
+
+  svg.append('g').call(xAxis);
+  svg.append('g').call(yAxis);
+
+  svg.node();
+})
